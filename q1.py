f9b815d6f1c97bcb605840317f4fbb34e128640f
boxman888 <epatterson888@hotmail.com>
2019-04-21 22:06:28 -0700

    Think I fixed my voting system

--- q1.py
+++ q1.py
@@ -3,6 +3,8 @@ from statistics import mode
 import math
 import numpy as np
 
+np.set_printoptions(suppress=True)
+
 class LoadData:
     def __init__(self, train, test):
         self.train = np.genfromtxt(train, delimiter=',')
@@ -14,13 +16,13 @@ class LoadData:
     def GetTrain(self):
         data = LoadData.Normalize(self.train)
         x = data[:,1:]
-        y = self.train[:,0]
+        y = x[:,0]
         return x, y
 
     def GetTest(self):
         data = LoadData.Normalize(self.test)
         x = data[:,1:]
-        y = self.test[:,0]
+        y = x[:,0]
         return x, y
 
 class KNN:
@@ -36,43 +38,29 @@ class KNN:
 
     def ABS_dist(v1, v2):
         return np.sum(np.absolute(v1 - v2))
+
     # Get Neighbors(Xtrain) near y, up to as K.
     def GetNeighbores(Xtrain, y, K):
         dist = []
         friends = []
 
-        # Store location of Xtrain[i] with distance in a tuple
-        # This will be used for looking up y's
         for i in range(Xtrain.shape[0]):
-            ed = KNN.Euclid_dist(Xtrain[i], y)
-            #ABS = KNN.ABS_dist(Xtrain[i],y)
-            dist.append((i, ed))
+            ABS = KNN.ABS_dist(Xtrain[i],y)
+            #ed = KNN.Euclid_dist(Xtrain[i], y)
+            dist.append((i, round(ABS, 3)))
 
-        # Sort tuples based on the euclid distance from k.
-        #dist.sort(key=KNN.SortTupleData)
-        #print(dist)
-        #print("before")
         dist.sort(key=lambda item: item[1])
-        #print("after")
-        #print(dist)
-        #print("zero")
-        #print(dist[0][0])
         for k in range(K):
             friends.append(dist[k][0])
 
         return friends
-
-    # Utility funciton used for GetNeighbors function.
-    #def SortTupleData(matrix):
-    #    return matrix[:][1];
 
     def LabelClass(neighbors, Ytrain):
         labelVote = []
         for i in neighbors:
             labelVote.append(Ytrain[i])
 
-        #print(neighbors)
-        quorum = mode(labelVote)
+        quorum = max(labelVote, key=labelVote.count)
         return quorum
 
     def CalculateKNN(self, K):
@@ -100,11 +88,10 @@ def main():
     FILES = LoadData(sys.argv[1], sys.argv[2])
 
     Xtrain, Ytrain = FILES.GetTrain()
-    Xtest, Ytest = FILES.GetTrain()
+    Xtest, Ytest = FILES.GetTest()
 
-    #print(Xtrain)
     knn = KNN(Xtrain, Ytrain, Xtest, Ytest)
-    tr_rep, te_rep = knn.CalculateKNN(51)
+    tr_rep, te_rep = knn.CalculateKNN(3)
 
     tr_e = knn.CalculateError(tr_rep, Ytrain)
     te_e = knn.CalculateError(te_rep, Ytest)
